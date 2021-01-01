775ab66a703e807fef649d3d806efaaa6edcbace
kunyi chen <41773722+aaeon-kunyi@users.noreply.github.com>
2021-01-01 16:39:12 +0800

    correct USER_ID for container

--- run_container.sh
+++ run_container.sh
@@ -8,7 +8,7 @@ export USER_ID
 export SHELL
 MOUNT_DIR=$(pwd)/works
 docker run --rm -i -t -v ${MOUNT_DIR}:/home/aaeon/works:rw \
-    -e USER_ID=${UID} -e GROUP_ID=${GID} -e TERM=xterm -e SHELL=${SHELL} \
+    -e USER_ID=${USER_ID} -e GROUP_ID=${GID} -e TERM=xterm -e SHELL=${SHELL} \
     -e http_proxy=$http_proxy -e https_proxy=$https_proxy -e no_proxy=$no_proxy \
     --privileged --device=/dev/net/tun:/dev/net/tun \
     --cap-add=NET_ADMIN \
